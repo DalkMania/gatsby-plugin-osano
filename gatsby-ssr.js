0742aaea9f31a1a1600d6904da50dcb63f4da441
Niklas Dahlqvist <dalkmania@gmail.com>
2020-12-18 16:49:56 -0800

    First Stab at the plugin

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,34 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
-// You can delete this file if you're not using it
+import React from "react"
+
+// Ensure that the Osano script gets loaded as early as it can. To comply with GDPR Rules
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  if (
+    process.env.NODE_ENV === `production` ||
+    pluginOptions.includeInDevelopment
+  ) {
+    const headComponents = getHeadComponents()
+    headComponents.sort((x, y) => {
+      if (x.key === 'gatsby-plugin-osano') {
+        return -1
+      } else if (y.key === 'gatsby-plugin-osano') {
+        return 1
+      }
+      return 0
+    })
+    replaceHeadComponents(headComponents)
+  }
+}
+
+export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+  if (
+    process.env.NODE_ENV === `production` ||
+    pluginOptions.includeInDevelopment
+  ) {
+    return setHeadComponents([
+      <script
+        key={`gatsby-plugin-osano`}
+        src={`https://cmp.osano.com/${pluginOptions.customerId}/${pluginOptions.ccid}/osano.js`}
+      />,
+    ])
+  }
+}
