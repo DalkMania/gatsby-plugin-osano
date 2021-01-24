5479bf2d27d796ea366bf14b5074d0ab932934ae
Niklas Dahlqvist <dalkmania@gmail.com>
2021-01-24 09:35:32 -0800

    Added Preload Link for Osano Script

--- src/__tests__/gatsby-ssr.js
+++ src/__tests__/gatsby-ssr.js
@@ -1,150 +1,147 @@
-import { onPreRenderHTML, onRenderBody } from "../gatsby-ssr"
+import { onPreRenderHTML, onRenderBody } from "../gatsby-ssr";
 
 describe(`gatsby-plugin-osano`, () => {
     describe(`onPreRenderHTML`, () => {
         describe(`in development mode`, () => {
             it(`it does not replace any head components, if the includeInDevelopment option is false`, () => {
-                const pluginOptions = { customerId:1, ccid:1,includeInDevelopment: false }
-                const getHeadComponents = jest.fn()
-                const replaceHeadComponents = jest.fn()
+                const pluginOptions = { customerId: 1, ccid: 1, includeInDevelopment: false };
+                const getHeadComponents = jest.fn();
+                const replaceHeadComponents = jest.fn();
 
-                onPreRenderHTML({ getHeadComponents, replaceHeadComponents }, pluginOptions)
-                expect(getHeadComponents).not.toHaveBeenCalled()
-                expect(replaceHeadComponents).not.toHaveBeenCalled()
-            })
+                onPreRenderHTML({ getHeadComponents, replaceHeadComponents }, pluginOptions);
+                expect(getHeadComponents).not.toHaveBeenCalled();
+                expect(replaceHeadComponents).not.toHaveBeenCalled();
+            });
 
             it(`it does replace head components, if the includeInDevelopment option is true and reorders the osano script to be first`, () => {
-                const pluginOptions = { customerId:1, ccid:1,includeInDevelopment: true }
+                const pluginOptions = { customerId: 1, ccid: 1, includeInDevelopment: true };
                 const components = [
                     {
-                        type: 'script',
-                        key: `script-1234`,
+                        type: "script",
+                        key: `script-1234`
                     },
                     {
-                        type: 'script',
-                        key: `gatsby-plugin-osano`,
+                        type: "script",
+                        key: `gatsby-plugin-osano`
                     }
-                ]
+                ];
                 const sortedComponents = [
                     {
-                        type: 'script',
-                        key: `gatsby-plugin-osano`,
+                        type: "script",
+                        key: `gatsby-plugin-osano`
                     },
                     {
-                        type: 'script',
-                        key: `script-1234`,
+                        type: "script",
+                        key: `script-1234`
                     }
-                ]
-                const getHeadComponents = jest.fn(() => components)
-                const replaceHeadComponents = jest.fn()
+                ];
+                const getHeadComponents = jest.fn(() => components);
+                const replaceHeadComponents = jest.fn();
 
-                onPreRenderHTML({ getHeadComponents, replaceHeadComponents }, pluginOptions)
+                onPreRenderHTML({ getHeadComponents, replaceHeadComponents }, pluginOptions);
 
-                expect(getHeadComponents.mock.calls).toMatchSnapshot()
-                expect(getHeadComponents).toHaveBeenCalledTimes(1)
-                expect(replaceHeadComponents).toHaveBeenCalledTimes(1)
-                expect(replaceHeadComponents).toHaveBeenCalledWith(sortedComponents)
+                expect(getHeadComponents.mock.calls).toMatchSnapshot();
+                expect(getHeadComponents).toHaveBeenCalledTimes(1);
+                expect(replaceHeadComponents).toHaveBeenCalledTimes(1);
+                expect(replaceHeadComponents).toHaveBeenCalledWith(sortedComponents);
+            });
+        });
 
-            })
-        })
-
-        describe('in production mode', () => {
-            let env
+        describe("in production mode", () => {
+            let env;
 
             beforeAll(() => {
-                env = process.env.NODE_ENV
-                process.env.NODE_ENV = `production`
-            })
+                env = process.env.NODE_ENV;
+                process.env.NODE_ENV = `production`;
+            });
 
             afterAll(() => {
-                process.env.NODE_ENV = env
-            })
+                process.env.NODE_ENV = env;
+            });
 
             it(`it does replace head components, in production and reorders the osano script to be first`, () => {
-                const pluginOptions = { customerId:1, ccid:1,includeInDevelopment: true }
+                const pluginOptions = { customerId: 1, ccid: 1, includeInDevelopment: true };
                 const components = [
                     {
-                        type: 'script',
-                        key: `script-1234`,
+                        type: "script",
+                        key: `script-1234`
                     },
                     {
-                        type: 'script',
-                        key: `gatsby-plugin-osano`,
+                        type: "script",
+                        key: `gatsby-plugin-osano`
                     }
-                ]
+                ];
                 const sortedComponents = [
                     {
-                        type: 'script',
-                        key: `gatsby-plugin-osano`,
+                        type: "script",
+                        key: `gatsby-plugin-osano`
                     },
                     {
-                        type: 'script',
-                        key: `script-1234`,
+                        type: "script",
+                        key: `script-1234`
                     }
-                ]
-                const getHeadComponents = jest.fn(() => components)
-                const replaceHeadComponents = jest.fn()
+                ];
+                const getHeadComponents = jest.fn(() => components);
+                const replaceHeadComponents = jest.fn();
 
-                onPreRenderHTML({ getHeadComponents, replaceHeadComponents }, pluginOptions)
+                onPreRenderHTML({ getHeadComponents, replaceHeadComponents }, pluginOptions);
 
-                expect(getHeadComponents.mock.calls).toMatchSnapshot()
-                expect(getHeadComponents).toHaveBeenCalledTimes(1)
-                expect(replaceHeadComponents).toHaveBeenCalledTimes(1)
-                expect(replaceHeadComponents).toHaveBeenCalledWith(sortedComponents)
-
-            })
-        })
-
-    })
+                expect(getHeadComponents.mock.calls).toMatchSnapshot();
+                expect(getHeadComponents).toHaveBeenCalledTimes(1);
+                expect(replaceHeadComponents).toHaveBeenCalledTimes(1);
+                expect(replaceHeadComponents).toHaveBeenCalledWith(sortedComponents);
+            });
+        });
+    });
 
     describe(`onRenderBody`, () => {
         describe(`in development mode`, () => {
             it(`does not set any head component, if the includeInDevelopment option is false`, () => {
-                const setHeadComponents = jest.fn()
-                const pluginOptions = { customerId:1, ccid:1,includeInDevelopment: false }
+                const setHeadComponents = jest.fn();
+                const pluginOptions = { customerId: 1, ccid: 1, includeInDevelopment: false };
 
-                onRenderBody({ setHeadComponents }, pluginOptions)
-                expect(setHeadComponents).not.toHaveBeenCalled()
-            })
+                onRenderBody({ setHeadComponents }, pluginOptions);
+                expect(setHeadComponents).not.toHaveBeenCalled();
+            });
 
             it(`does set head component, if the includeInDevelopment option is true`, () => {
-                const setHeadComponents = jest.fn()
-                const pluginOptions = { customerId:1, ccid:1,includeInDevelopment: true }
+                const setHeadComponents = jest.fn();
+                const pluginOptions = { customerId: 1, ccid: 1, includeInDevelopment: true };
 
-                onRenderBody({ setHeadComponents }, pluginOptions)
-                expect(setHeadComponents.mock.calls).toMatchSnapshot()
-                expect(setHeadComponents).toHaveBeenCalledTimes(1)
+                onRenderBody({ setHeadComponents }, pluginOptions);
+                expect(setHeadComponents.mock.calls).toMatchSnapshot();
+                expect(setHeadComponents).toHaveBeenCalledTimes(1);
                 expect(setHeadComponents).toHaveBeenCalledWith([
-                    expect.objectContaining({ key: `gatsby-plugin-osano` }),
-                ])
-            })
-        })
+                    expect.objectContaining({ key: `preload-gatsby-plugin-osano` }),
+                    expect.objectContaining({ key: `gatsby-plugin-osano` })
+                ]);
+            });
+        });
 
         describe(`in production mode`, () => {
-            let env
+            let env;
 
             beforeAll(() => {
-                env = process.env.NODE_ENV
-                process.env.NODE_ENV = `production`
-            })
+                env = process.env.NODE_ENV;
+                process.env.NODE_ENV = `production`;
+            });
 
             afterAll(() => {
-                process.env.NODE_ENV = env
-            })
+                process.env.NODE_ENV = env;
+            });
 
             it(`does set head component`, () => {
-                const setHeadComponents = jest.fn()
-                const pluginOptions = { customerId:1, ccid:1 }
+                const setHeadComponents = jest.fn();
+                const pluginOptions = { customerId: 1, ccid: 1 };
 
-                onRenderBody({ setHeadComponents }, pluginOptions)
-                expect(setHeadComponents.mock.calls).toMatchSnapshot()
-                expect(setHeadComponents).toHaveBeenCalledTimes(1)
+                onRenderBody({ setHeadComponents }, pluginOptions);
+                expect(setHeadComponents.mock.calls).toMatchSnapshot();
+                expect(setHeadComponents).toHaveBeenCalledTimes(1);
                 expect(setHeadComponents).toHaveBeenCalledWith([
-                    expect.objectContaining({ key: `gatsby-plugin-osano` }),
-                ])
-            })
-
-
-        })
-    })
-})
+                    expect.objectContaining({ key: `preload-gatsby-plugin-osano` }),
+                    expect.objectContaining({ key: `gatsby-plugin-osano` })
+                ]);
+            });
+        });
+    });
+});
--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,34 +1,35 @@
-import React from "react"
+import React from "react";
 
 // Ensure that the Osano script gets loaded as early as it can. To comply with GDPR Rules
 export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }, pluginOptions) => {
-  if (
-    process.env.NODE_ENV === `production` ||
-    pluginOptions.includeInDevelopment
-  ) {
-    const headComponents = getHeadComponents()
-    headComponents.sort((x, y) => {
-      if (y.type === 'script' && x.key === 'gatsby-plugin-osano') {
-        return -1
-      } else if (x.type === 'script' && y.key === 'gatsby-plugin-osano') {
-        return 1
-      }
-      return 0
-    })
-    replaceHeadComponents(headComponents)
-  }
-}
+    if (process.env.NODE_ENV === `production` || pluginOptions.includeInDevelopment) {
+        const headComponents = getHeadComponents();
+        headComponents.sort((x, y) => {
+            if (y.type === "script" && x.key === "gatsby-plugin-osano") {
+                return -1;
+            } else if (x.type === "script" && y.key === "gatsby-plugin-osano") {
+                return 1;
+            }
+            return 0;
+        });
+        replaceHeadComponents(headComponents);
+    }
+};
 
 export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  if (
-    process.env.NODE_ENV === `production` ||
-    pluginOptions.includeInDevelopment
-  ) {
-    return setHeadComponents([
-      <script
-        key={`gatsby-plugin-osano`}
-        src={`https://cmp.osano.com/${pluginOptions.customerId}/${pluginOptions.ccid}/osano.js`}
-      />,
-    ])
-  }
-}
+    if (process.env.NODE_ENV === `production` || pluginOptions.includeInDevelopment) {
+        return setHeadComponents([
+            <link
+                key={`preload-gatsby-plugin-osano`}
+                rel="preload"
+                as="script"
+                href={`https://cmp.osano.com/${pluginOptions.customerId}/${pluginOptions.ccid}/osano.js`}
+            />,
+            <script
+                key={`gatsby-plugin-osano`}
+                src={`https://cmp.osano.com/${pluginOptions.customerId}/${pluginOptions.ccid}/osano.js`}
+                async
+            />
+        ]);
+    }
+};
